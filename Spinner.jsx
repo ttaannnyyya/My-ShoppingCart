5705eb80fb12d7f7eed3a57ddbac0f413d35ec15
Tanya Goswami <112064555+ttaannnyyya@users.noreply.github.com>
2024-12-15 21:23:38 +0530

    Add files via upload

---
+++ Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Spinner.css"
+
+const Spinner = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className='spinner top-24'></div>
+    </div>
+  )
+}
+
+export default Spinner
